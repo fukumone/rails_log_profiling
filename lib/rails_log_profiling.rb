30c3767a28809ea35824a10529704dafc0240170
fukumone <fukuimone@gmail.com>
2016-10-17 22:23:43 +0900

    active recordのversionで処理を分ける

--- lib/rails_log_profiling.rb
+++ lib/rails_log_profiling.rb
@@ -42,19 +42,56 @@ module Rails
 
           return if IGNORE_PAYLOAD_NAMES.include?(payload[:name])
 
-          name  = "#{payload[:name]} (#{event.duration.round(1)}ms)"
-          sql   = payload[:sql]
-          binds = nil
+          if ActiveRecord.version >= Gem::Version.new("5.0.0.beta")
+            ar_ver_5(event)
+          else
+            ar_ver_4(event)
+          end
+        end
 
-          unless (payload[:binds] || []).empty?
-            binds = "  " + payload[:binds].map { |attr| render_bind(attr) }.inspect
+        private
+
+          def ar_ver_5(event)
+            payload = event.payload
+            name  = "#{payload[:name]} (#{event.duration.round(1)}ms)"
+            sql   = payload[:sql]
+            binds = nil
+
+            unless (payload[:binds] || []).empty?
+              binds = "  " + payload[:binds].map { |attr| render_bind(attr) }.inspect
+            end
+
+            name = colorize_payload_name(name, payload[:name])
+            sql  = color(sql, sql_color(sql), true)
+
+            Rails::Log::Profiling.sqls << [ "#{event.duration.round(1)}".to_f, "  #{name}  #{sql}#{binds}"]
           end
 
-          name = colorize_payload_name(name, payload[:name])
-          sql  = color(sql, sql_color(sql), true)
+          def ar_ver_4(event)
+            payload = event.payload
+            name  = '%s (%.1fms)' % [payload[:name], event.duration]
+            sql   = payload[:sql].squeeze(' ')
+            binds = nil
 
-          Rails::Log::Profiling.sqls << [ "#{event.duration.round(1)}".to_f, "  #{name}  #{sql}#{binds}"]
-        end
+            unless (payload[:binds] || []).empty?
+              binds = "  " + payload[:binds].map { |col,v|
+                if col
+                  [col.name, v]
+                else
+                  [nil, v]
+                end
+              }.inspect
+            end
+
+            if odd?
+              name = color(name, CYAN, true)
+              sql  = color(sql, nil, true)
+            else
+              name = color(name, MAGENTA, true)
+            end
+
+            Rails::Log::Profiling.sqls << [ "#{event.duration.round(1)}".to_f, "  #{name}  #{sql}#{binds}" ]
+          end
       end
 
       class ActionLogSubscriber < ActionController::LogSubscriber
@@ -62,7 +99,7 @@ module Rails
           return unless Rails::Log::Profiling.logger.debug?
           payload = event.payload
           if Rails::Log::Profiling.logger_ok
-            Rails::Log::Profiling.logger.debug "#{payload[:controller]}##{payload[:action]}"
+            Rails::Log::Profiling.logger.debug "\n \033[36m#{payload[:controller]}##{payload[:action]}"
           end
           Rails::Log::Profiling.logger_ok = true
         end
