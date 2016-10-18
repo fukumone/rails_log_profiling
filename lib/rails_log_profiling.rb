426f42b2505f84ab374c76a2903a1de97f0ad0b7
fukumone <fukuimone@gmail.com>
2016-10-18 22:08:51 +0900

    versionごとのログ修正

--- lib/rails_log_profiling.rb
+++ lib/rails_log_profiling.rb
@@ -19,10 +19,6 @@ module Rails
         []
       end
 
-      config_accessor :logger_ok, instance_accessor: false do
-        false
-      end
-
       config_accessor :sqls, instance: false, instance_accessor: false  do
         []
       end
@@ -64,7 +60,9 @@ module Rails
             name = colorize_payload_name(name, payload[:name])
             sql  = color(sql, sql_color(sql), true)
 
-            Rails::Log::Profiling.sqls << [ "#{event.duration.round(1)}".to_f, "  #{name}  #{sql}#{binds}"]
+            if !name.match(/.*ActiveRecord::SchemaMigration.*/) && name.match(/.*Load.\(.*ms\).*/)
+              Rails::Log::Profiling.sqls << [ "#{event.duration.round(1)}".to_f, "  #{name}  #{sql}#{binds}"]
+            end
           end
 
           def ar_ver_4(event)
@@ -90,38 +88,35 @@ module Rails
               name = color(name, MAGENTA, true)
             end
 
-            Rails::Log::Profiling.sqls << [ "#{event.duration.round(1)}".to_f, "  #{name}  #{sql}#{binds}" ]
+            if !name.match(/.*ActiveRecord::SchemaMigration.*/) && name.match(/.*Load.\(.*ms\).*/)
+              Rails::Log::Profiling.sqls << [ "#{event.duration.round(1)}".to_f, "  #{name}  #{sql}#{binds}" ]
+            end
           end
       end
+    end
+  end
+end
 
-      class ActionLogSubscriber < ActionController::LogSubscriber
-        def start_processing(event)
-          return unless Rails::Log::Profiling.logger.debug?
-          payload = event.payload
-          if Rails::Log::Profiling.logger_ok
-            Rails::Log::Profiling.logger.debug "\n \033[36m#{payload[:controller]}##{payload[:action]}"
-          end
-          Rails::Log::Profiling.logger_ok = true
+if defined?(Rails)
+  module ActionController
+    class Base
+      include Rails::Log::Profiling
+      before_filter :controller_action_message
+      after_filter :logger_execute
+
+      private
+        def controller_action_message
+          controller_name = params[:controller].split("_").map(&:capitalize).join
+          Rails::Log::Profiling.logger.info("#{controller_name}Controller##{params[:action]}")
         end
 
-        def process_action(event)
-          return unless Rails::Log::Profiling.logger.debug?
+        def logger_execute
           Rails::Log::Profiling::QueryProfiling.execute
-          Rails::Log::Profiling.logger_ok = false
         end
-      end
-
-      # class ViewLogSubscriber < ActionView::LogSubscriber
-      #   # TODO: loggerの処理を書く、rails_log_profiling.logに書いていく処理を追加
-      # end
     end
   end
 end
 
 ActiveSupport.on_load(:active_record) do
   Rails::Log::Profiling::QueryLogSubscriber.attach_to :active_record
-end
-
-ActiveSupport.on_load(:action_controller) do
-  Rails::Log::Profiling::ActionLogSubscriber.attach_to :action_controller
 end
