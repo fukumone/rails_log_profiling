9333e192d9f3ea8728030e80ee839a68da97100b
fukumone <fukuimone@gmail.com>
2016-10-19 20:38:22 +0900

    クエリの検知数表示

--- lib/rails_log_profiling.rb
+++ lib/rails_log_profiling.rb
@@ -107,10 +107,11 @@ if defined?(Rails)
       private
         def controller_action_message
           controller_name = params[:controller].split("_").map(&:capitalize).join
-          Rails::Log::Profiling.logger.info("#{controller_name}Controller##{params[:action]}")
+          Rails::Log::Profiling.logger.info("\n \033[36m #{controller_name}Controller##{params[:action]}")
         end
 
         def logger_execute
+          Rails::Log::Profiling.logger.info("\n \033[36m" + Rails::Log::Profiling.sqls.count.to_s + "件のクエリの検知")
           Rails::Log::Profiling::QueryProfiling.execute
         end
     end
