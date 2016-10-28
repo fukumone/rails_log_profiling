9b156a4278aaf420587eabf4f98f41d81a8cd0ec
fukumone <fukuimone@gmail.com>
2016-10-28 21:17:54 +0900

    action名を変更

--- lib/rails/log/profiling/action_controller.rb
+++ lib/rails/log/profiling/action_controller.rb
@@ -3,10 +3,10 @@ require 'action_controller'
 module ActionController
   class Base
     include Rails::Log::Profiling
-    after_action :logger_execute
+    after_action :rails_query_profiling_logger_execute
 
     private
-      def logger_execute
+      def rails_query_profiling_logger_execute
         unless Rails::Log::Profiling.sqls.empty?
           controller_name = params[:controller].split("_").map(&:capitalize).join
           Rails::Log::Profiling.logger.info("\n\033[36m #{controller_name}Controller##{params[:action]}")
