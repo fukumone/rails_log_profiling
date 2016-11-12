03b672d920512a88d5cb89bb2db1f97d01be5eac
fukumone <fukuimone@gmail.com>
2016-11-12 16:30:34 +0900

    0.1.1リリース、バグfix

--- spec/unit/version_spec.rb
+++ spec/unit/version_spec.rb
@@ -2,6 +2,6 @@ require "spec_helper"
 
 describe Rails::Log::Profiling do
   it "has a version number" do
-    expect(Rails::Log::Profiling::VERSION).to eq("0.1.0")
+    expect(Rails::Log::Profiling::VERSION).to eq("0.1.1")
   end
 end
